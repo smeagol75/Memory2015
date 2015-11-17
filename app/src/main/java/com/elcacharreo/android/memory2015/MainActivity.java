f318ddabc419b37e9bb5f69b60b1aadfe559075d
javacasm <javacasm@gmail.com>
2015-11-17 19:12:58 +0100

    Initial commit

---
+++ app/src/main/java/com/elcacharreo/android/memory2015/MainActivity.java
@@ -0,0 +1,87 @@
+package com.elcacharreo.android.memory2015;
+
+import android.media.Image;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    int []imagenBoton={R.drawable.conejo,R.drawable.oveja,R.drawable.pollo,
+            R.drawable.conejo,R.drawable.oveja,R.drawable.pollo,
+            R.drawable.rinoceronte,R.drawable.serpiente,R.drawable.tiburon,
+            R.drawable.rinoceronte,R.drawable.serpiente,R.drawable.tiburon
+    };  // Guarda la imagen de cada boton
+    int []idBoton={R.id.iv11,R.id.iv12,R.id.iv13,
+                  R.id.iv21,R.id.iv22,R.id.iv23,
+                  R.id.iv31,R.id.iv32,R.id.iv33,
+                  R.id.iv41,R.id.iv42,R.id.iv43}; // Guarda el id de los botones
+
+    int iPuntuacion=0;
+    int iTiempo=0;
+    void inicializacion()
+    {
+        // TODO: rellenar aleatoriamente (rellenar imagenBoton )
+
+        // TODO: cargar sonidos
+
+        // TODO: puntuacion a 0
+        iPuntuacion=0;
+
+        // TODO: tiempo a 0
+        iTiempo=0;
+    }
+
+    public void botonStart(View v)
+    {
+        inicializacion();
+
+        // TODO: hacer visible Las cartas (tablelayout.setVisibility(Visible) );
+
+    }
+
+    public void clickImagen(View v)
+    {
+
+        // TODO: Buscar boton para ver su imagen
+
+        int iNumeroBotonPulsado=0;
+        for(int i=0;i<12;i=i+1)
+        {
+            if(v.getId()==idBoton[i])
+            {
+                iNumeroBotonPulsado=i;
+                break;
+            }
+        }
+
+
+        ImageView ivPulsado=(ImageView)v;
+
+        // TODO: Mostrar imagen (iv.setImageResource(R.drawable.IMAGEN); )
+        ivPulsado.setImageResource(imagenBoton[iNumeroBotonPulsado]);
+
+
+
+    }
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
